8789512f1b7e0ae7a336abe55ab6c496a8fa12ca
Foster Gailey <foster@gaileyhome.com>
2019-09-29 12:50:13 -0400

    Did alot of css, left bar looks ok

--- assets/javascript/design.js
+++ assets/javascript/design.js
@@ -1,5 +1,7 @@
 let topics = ['jordan cry', 'harambe', 'john wick', 'record egg', 'sponge mock', 'jason bourne', 'covfefe'];
 let titleDisplay = String;
+let remove = true;
+
 
 $( document ).ready(function() {
 
@@ -7,9 +9,14 @@ $( document ).ready(function() {
     //adds new button based on typed info
     $("#run-search").on("click", searchButton);
     //searches for meme
-    $(document).on("click", ".meme-button", memeSearch);
+    $(document).on("click", ".topic-btn", topicSearch);
     //animate or still the mem
     $(document).on("click", ".animate", animateGif);
+    //removal button on click allow meme buttons to be deleted
+    $("#removal").on("click", removal)
+
+    //closes the right side bar
+    $("#toggle-left").on("click", leftBarClose);
     
     //*******************************************************
 
@@ -24,7 +31,7 @@ $( document ).ready(function() {
         else {
             alert("You must write something")
         }
-
+        $("#search-term").val('')
         console.log(topics)
        
         renderButton();
@@ -35,15 +42,20 @@ $( document ).ready(function() {
         $("#gifButtons").empty();
         for (let x = 0; x < topics.length; x++){
             let newButton = $("<button>");
+
+            let bg = ["#00FFFF", "#00CCFF", "#7CB9E8", "#0048BA", "#ACE5EE"];
+            let gen = bg[Math.floor(Math.random() * bg.length)];
+
+            newButton.css("background-color", gen)
             newButton.attr("data-name", topics[x]);
-            newButton.addClass("meme-button");
+            newButton.addClass("topic-btn btn btn-outline-dark");
             newButton.text(topics[x]);
             $("#gifButtons").append(newButton)
         }
     }
     renderButton();
 
-
+    //changes gif from still to active
     function animateGif(){
         if ($(this).attr("data-status")==="still"){
             console.log("still")
@@ -57,64 +69,91 @@ $( document ).ready(function() {
     }
     
     //function for when meme button is pressed
-    function memeSearch() {
+    function topicSearch() {
 
         event.preventDefault();
-        
-        // Here we grab the text from the input box
-        let search = $(this).attr("data-name")
-        // Here we construct our URL\
-        console.log($(".meme-button"))
-        let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=lDSzVvM7FYinoWf9vRVrIn97y8WQVNHU&q=${search}&limit=10`;
+        if (remove){
+            // Here we grab the text from the input box
+            let search = $(this).attr("data-name")
+            // Here we construct our URL\
+            console.log($(".topic-btn"))
+            let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=lDSzVvM7FYinoWf9vRVrIn97y8WQVNHU&q=${search}&limit=10`;
 
-        $.ajax({
-        url: queryURL,
-        method: "GET",
-        }).then(function(response){
-        let results = response.data;
-        console.log(response)
-        for(var i = 0; i < results.length; i++){
-            let newDiv = $("<div>");
-            newDiv.addClass("float-left text-center m-2 border border-dark")
-            $("#content").prepend(newDiv);
-            
-            //removes everything after from gif and later then capitilizes the words
-            let gifTitle = results[i].title
-            let n = gifTitle.search("GIF")            
-            str = gifTitle.slice(0,n)
+            $.ajax({
+            url: queryURL,
+            method: "GET",
+            }).then(function(response){
+            let results = response.data;
+            console.log(response)
+            for(var i = 0; i < results.length; i++){
+                let newDiv = $("<div>");
+                newDiv.addClass("float-left text-center border border-dark")
+                $("#content").prepend(newDiv);
+                
+                //removes everything after from gif and later then capitilizes the words
+                let gifTitle = results[i].title
+                let n = gifTitle.search("GIF")            
+                str = gifTitle.slice(0,n)
 
-            if (str.search(" ")){
-                str = str.split(" ");
+                if (str.search(" ")){
+                    str = str.split(" ");
 
-                for (var c = 0, x = str.length; c < x; c++) {
-                str[c] = str[c].charAt(0).toUpperCase() + str[c].substr(1);
+                    for (var c = 0, x = str.length; c < x; c++) {
+                    str[c] = str[c].charAt(0).toUpperCase() + str[c].substr(1);
+                    }
+
+                    titleDisplay = str.join(" ");
                 }
 
-                titleDisplay = str.join(" ");
+                //adds the new title to the gif
+                let newTitle = $("<div>");
+                newTitle.text(`${titleDisplay}`);
+                $(newDiv).append(newTitle);
+
+                let newImg = $("<img>");
+                newImg.attr("src", results[i].images.fixed_height_small_still.url);
+                newImg.attr("data-still", results[i].images.fixed_height_small_still.url);
+                newImg.attr("data-active", results[i].images.fixed_height_small.url);
+                newImg.attr("data-status", "still");
+                newImg.addClass("animate");
+                newImg.attr("draggable", true)
+                $(newDiv).append(newImg)
+
+
+                let newRating = $("<div>");
+                newRating.text(`Results: ${results[i].rating.toUpperCase()}`);
+                $(newDiv).append(newRating)
             }
-
-            //adds the new title to the gif
-            let newTitle = $("<div>");
-            newTitle.text(`${titleDisplay}`);
-            $(newDiv).append(newTitle);
-
-            let newImg = $("<img>");
-            newImg.attr("src", results[i].images.fixed_height_still.url);
-            newImg.attr("data-still", results[i].images.fixed_height_still.url);
-            newImg.attr("data-active", results[i].images.fixed_height.url);
-            newImg.attr("data-status", "still");
-            newImg.addClass("animate");
-            newImg.attr("draggable", true)
-            $(newDiv).append(newImg)
-
-
-            let newRating = $("<div>");
-            newRating.text(`Results: ${results[i].rating.toUpperCase()}`);
-            $(newDiv).append(newRating)
+            });
+                
         }
-        });
-            
+        else {
+            $(this).remove();
+        }
     };
 
+    //alternates removal button
+    function removal(){
+        console.log("working")
+        if (remove){
+            console.log(remove)
+            remove = false;
+            $("#removal").removeClass("btn-dark");
+            $("#removal").addClass("btn-danger");
+            console.log(remove)
+        }else {
+            console.log(remove)
+            remove = true;
+            console.log(remove)
+            $("#removal").removeClass("btn-danger");
+            $("#removal").addClass("btn-dark");
+        }
+    }
 
+    
+    function leftBarClose(){
+        $(".main").css('marginLeft',"0%");
+        $("#left-bar").css('display',"none");
+        // $("#open-left").css('display',"inline-block");
+    }
 });
